3d91ed4dc975b2011de691ce374a59ef5ccbba58
Dactarbari <api.dactarbari@gmail.com>
2019-06-22 18:41:50 +0600

    marge testing

--- app/src/main/java/com/twopibd/dactarbari/doctor/appointment/Activity/LoginActivity.java
+++ app/src/main/java/com/twopibd/dactarbari/doctor/appointment/Activity/LoginActivity.java
@@ -48,8 +48,6 @@ public class LoginActivity extends AppCompatActivity implements ApiListener.Logi
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Please wait");
 
-        Toast.makeText(this, "ok111 cha 2 pu   sgdfgdhed", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "dghdjhgkdhf d gdkfg   ", Toast.LENGTH_SHORT).show();
 
 
 
