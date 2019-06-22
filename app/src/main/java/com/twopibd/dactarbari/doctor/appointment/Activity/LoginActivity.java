4d4da6c4d4ba1a024781b3c75f7c17efeecb0c63
mukul2 <saidur.shawon@gmail.com>
2019-06-22 21:28:23 +0600

    Toast msg Change

--- app/src/main/java/com/twopibd/dactarbari/doctor/appointment/Activity/LoginActivity.java
+++ app/src/main/java/com/twopibd/dactarbari/doctor/appointment/Activity/LoginActivity.java
@@ -47,7 +47,7 @@ public class LoginActivity extends AppCompatActivity implements ApiListener.Logi
         ButterKnife.bind(this);
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Please wait");
-        Toast.makeText(this, "zz3 Mukul......", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "zz3 Mukul......NEw branch.....", Toast.LENGTH_SHORT).show();
 
 
 
